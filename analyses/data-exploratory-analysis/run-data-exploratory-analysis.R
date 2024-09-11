768278bfbeaf1202ed593d9c8c9f9c66a7c1c8a4
Chroni <achroni@splprhpc11.cm.cluster>
2024-09-11 14:21:56 -0500

    Edit README file and R script

--- analyses/data-exploratory-analysis/run-data-exploratory-analysis.R
+++ analyses/data-exploratory-analysis/run-data-exploratory-analysis.R
@@ -9,18 +9,6 @@ report_dir <- file.path(analysis_dir, "plots")
 ################################################################################################################
 # Run Rmd script
 ################################################################################################################
-rmarkdown::render('01-create-cohort.Rmd', clean = TRUE,
-                                          output_dir = file.path(report_dir),
-                                          output_file = c(paste('Report-', 'create-cohort', '-', Sys.Date(), sep = '')),
-                                          output_format = 'all',
-                                          params = list(input_file = "demo-data.csv",
-
-                                                        # the following parameters should be similar across the module
-                                                        PROJECT_NAME = "Comprehensive Omics Catalogue for Hartwell",
-                                                        START_DATE = "09/04/2024",
-                                                        COMPLETION_DATE = "9/06/2024"))
-################################################################################################################
-################################################################################################################
 rmarkdown::render('02-data-exploratory-analysis.Rmd', clean = TRUE,
                                                       output_dir = file.path(report_dir),
                                                       output_file = c(paste('Report-', 'data-exploratory-analysis', '-', Sys.Date(), sep = '')),
